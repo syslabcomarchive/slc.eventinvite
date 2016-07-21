0a74f804e71641a3e3409b75343c00b0f3f730f9
Alexander Pilz <pilz@syslab.com>
2016-07-21 11:56:12 +0200

    import fix

--- slc/eventinvite/browser/invite.py
+++ slc/eventinvite/browser/invite.py
@@ -19,12 +19,10 @@ from plone.z3cform.layout import FormWrapper
 from collective.z3cform.datagridfield import DataGridFieldFactory
 from collective.z3cform.datagridfield import DictRow
 
-from Products.UserAndGroupSelectionWidget.z3cform.widget import \
-                                        UsersAndGroupsSelectionFieldWidget
-
 from slc.eventinvite import MessageFactory as _
 from slc.eventinvite import utils 
 from slc.eventinvite.adapter import IAttendeesStorage
+
 
 def isEmail(value):
     if re.match('^'+EMAIL_RE, value):
@@ -39,46 +37,46 @@ class IExternalAttendee(form.Schema):
     """ External attendees are non-Plone users who will be attending the event.
     """
     name = schema.TextLine(
-                    title=_(u"Name"),
-                    required=True,
-                    )
+        title=_(u"Name"),
+        required=True,
+    )
 
     email = schema.TextLine(
-                    title=_(u"Email"), 
-                    constraint=isEmail,
-                    required=True,
-                    )
+        title=_(u"Email"), 
+        constraint=isEmail,
+        required=True,
+    )
 
 
 class IEventInviteFormSchema(interface.Interface):
     """ """
     internal_attendees = schema.List(
-                title=_(u"Internal attendees"),
-                description=_(u"help_internal_attendees",
-                        default=u"Here you can choose attendees "
-                        u"from users in this system."),
-                value_type=schema.TextLine(),
-                required=False,
-                missing_value=list(),
-            )
+        title=_(u"Internal attendees"),
+        description=_(u"help_internal_attendees",
+                      default=u"Here you can choose attendees "
+                              u"from users in this system."),
+        value_type=schema.TextLine(),
+        required=False,
+        missing_value=list(),
+    )
 
     external_attendees = schema.List(
-                title=_(u"External attendees"),
-                description=_(u"help_external_attendees",
-                        default=u"Here you can provide details about attendees "
-                        u"who are NOT users of this system."),
-                value_type=DictRow(title=u"External Attendee", schema=IExternalAttendee),
-                required=False,
-                missing_value=list(),
-            )
+        title=_(u"External attendees"),
+        description=_(u"help_external_attendees",
+                      default=u"Here you can provide details about attendees "
+                              u"who are NOT users of this system."),
+        value_type=DictRow(title=u"External Attendee", schema=IExternalAttendee),
+        required=False,
+        missing_value=list(),
+    )
 
 
 def show_email_new_button(form):
-    """ """ 
+    """ """
     context = form.context
     storage = IAttendeesStorage(context)
     attendees = storage.get('internal_attendees', []) + \
-                storage.get('external_attendees', [])
+        storage.get('external_attendees', [])
     if not attendees:
         return False
     return True
@@ -88,8 +86,8 @@ class IEventInviteFormButtons(interface.Interface):
     """ """
     email_all = button.Button(title=_(u"Save and email all attendees"))
     email_new = button.Button(
-                    title=_(u"Save and email only the attendees that were added now"),
-                    condition=show_email_new_button)
+        title=_(u"Save and email only the attendees that were added now"),
+        condition=show_email_new_button)
 
 
 class EventInviteForm(ExtensibleForm, z3cform.Form):
@@ -101,8 +99,6 @@ class EventInviteForm(ExtensibleForm, z3cform.Form):
     def updateFields(self):
         super(EventInviteForm, self).updateFields()
         self.fields['external_attendees'].widgetFactory = DataGridFieldFactory
-        self.fields['internal_attendees'].widgetFactory = \
-                                            UsersAndGroupsSelectionFieldWidget
 
     def updateWidgets(self):
         """ Make sure that the widgets are populated with stored or request
@@ -118,7 +114,7 @@ class EventInviteForm(ExtensibleForm, z3cform.Form):
                     widget_value = [k['id'] for k in storage.get(key, [])]
                 else:
                     widget_value = storage.get(key)
-            if widget_value: 
+            if widget_value:
                 self.widgets[key].value = widget_value
 
     @button.handler(IEventInviteFormButtons['email_all'])
@@ -126,12 +122,12 @@ class EventInviteForm(ExtensibleForm, z3cform.Form):
         data, errors = self.extractData()
         if errors:
             self.status = '\n'.join([error.error.__str__() for error in errors])
-            return 
+            return
         data = utils.save_attendees(self.context, data)
         utils.email_recipients(self.context, data)
-        addStatusMessage(self.request, 
-                        "Attendees have been saved and notified",
-                        type='info')
+        addStatusMessage(self.request,
+                         "Attendees have been saved and notified",
+                         type='info')
         self.request.response.redirect(self.context.REQUEST.get('URL'))
 
     @button.handler(IEventInviteFormButtons['email_new'])
@@ -139,7 +135,7 @@ class EventInviteForm(ExtensibleForm, z3cform.Form):
         data, errors = self.extractData()
         if errors:
             self.status = '\n'.join([error.error.__str__() for error in errors])
-            return 
+            return
 
         context = aq_inner(self.context)
         new_attendees = utils.get_new_attendees(context, data)
@@ -147,13 +143,12 @@ class EventInviteForm(ExtensibleForm, z3cform.Form):
         utils.email_recipients(context, new_attendees)
         if new_attendees['internal_attendees'] or \
                 new_attendees['external_attendees']:
-            addStatusMessage(self.request, 
-                            "The new attendees have been added and notified.",
-                            type='info')
+            addStatusMessage(self.request,
+                             "The new attendees have been added and notified.",
+                             type='info')
         self.request.response.redirect(self.context.REQUEST.get('URL'))
 
 
 class EventInvite(FormWrapper):
     """ """
-    form = EventInviteForm 
-
+    form = EventInviteForm
